b1068fb2f9b055ff20895ce0b5576d265134fde7
Eduardo Lemos Martins <eduardol_martins@hotmail.com>
2020-12-24 13:28:37 -0300

    Handiling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,6 +31,11 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	
+	public static void clearScreen() {
+		System.out.println("\033[h\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 		String s = sc.nextLine();
@@ -51,7 +56,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println("   a  b  c  d  e  f  g  h");
 	}
 
 	public static void printPiece(ChessPiece piece) {
