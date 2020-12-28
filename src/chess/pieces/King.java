2a0143866a846f87a51d1fc8eb3aa3517c51d864
Eduardo Lemos Martins <eduardol_martins@hotmail.com>
2020-12-27 21:55:45 -0300

    Special move - Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,24 +2,35 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch;
+
+	public King(Board board, Color color, ChessMatch chessmate) {
 		super(board, color);
+		this.chessMatch = chessmate;
 	}
 
 	@Override
 	public String toString() {
 		return "K";
 	}
-	
+
 	private boolean canMove(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
-		return p == null || p.getColor() !=getColor();
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
 	}
+
+	private boolean testRoockCastling(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
+
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
@@ -90,6 +101,29 @@ public class King extends ChessPiece{
 			
 		}
 		
+		//#specialmove Castling
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			//	@specialmove Castling Kingside rook
+			Position posT1 = new Position(position.getRow(),position.getColumn()+3);
+			if(testRoockCastling(posT1)) {
+				Position p1 = new Position(position.getRow(),position.getColumn()+1);
+				Position p2 = new Position(position.getRow(),position.getColumn()+2);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					mat[position.getRow()][position.getColumn()+2] = true;
+				}
+			}
+
+				//	@specialmove Castling Queenside rook
+				Position posT2 = new Position(position.getRow(),position.getColumn()-4);
+				if(testRoockCastling(posT2)) {
+					Position p1 = new Position(position.getRow(),position.getColumn()-1);
+					Position p2 = new Position(position.getRow(),position.getColumn()-2);
+					Position p3 = new Position(position.getRow(),position.getColumn()-3);
+					if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+						mat[position.getRow()][position.getColumn()-2] = true;
+					}
+				}
+		}
 		
 		return mat;
 	}
